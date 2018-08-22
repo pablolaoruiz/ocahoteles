9652f399e843903624fa10db68de29f03199597a
Juan Antonio Madueño <juanantonio@geometric.me>
2018-08-22 12:45:42 +0200

    test

--- 404.php
+++ 404.php
@@ -1,16 +1,16 @@
 <?php
 
     require('startPage.inc');
-    
+
     $destinos = $ctrlHoteles->getDestinos();
-  
+
     $tmplCuerpo = "tmpl_normal";
     $seccion = "404";
-    
-    $class_menu = "menu-interior";
+
+    $class_menu = "meddddnu-interior";
     $titulo =  "404";
     $descripcion =   "404";
 
     require(getTmpl('tmpl_main','main'));
-    
+
 ?>
